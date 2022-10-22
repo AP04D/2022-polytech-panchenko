bf6927d7c4d2ba68dd99b4169f894eb5d250c55c
AP04D <mrandpan2004@mail.ru>
2022-10-22 13:49:44 +0300

    feat: function one

---
+++ 04_search_sort/array_insertion/array_insention.cpp
@@ -0,0 +1,21 @@
+int* insert(int arr[], int n, int x, int p) {
+    for (auto i = n; i > p; i--)
+        arr[i] = arr[i - 1];
+
+    arr[p] = x;
+    return arr;
+}
+
+
+int main()
+{
+    // `insert` example
+    int a0[10] = { 0, 1, 2, 3,      4, 5, 6, 7, 8, 9 };
+    int expected_r0[11] = { 0, 1, 2, 3, 100, 4, 5, 6, 7, 8, 9 };
+
+    auto r0 = insert(a0, 10, 100, 4);
+    for (auto i = 0; i < 11; ++i)
+        assert(r0[i] == expected_r0[i]);
+
+    return 0;
+}
