48b50066f1a36a18259ca05d95d9ac8f792aa9a8
AP04D <mrandpan2004@mail.ru>
2022-10-29 10:52:55 +0300

    feat: search position added

--- 04_search_sort/array_insertion/array_insention.cpp
+++ 04_search_sort/array_insertion/array_insention.cpp
@@ -6,6 +6,13 @@ int* insert(int arr[], int n, int x, int p) {
     return arr;
 }
 
+int search_position(int arr[], int n, int x) {
+    for (auto i = 0; i < n - 1; ++i) {
+        if (arr[i] >= x)
+            return i;
+    }
+    return n;
+}
 
 int main()
 {
@@ -16,6 +23,13 @@ int main()
     auto r0 = insert(a0, 10, 100, 4);
     for (auto i = 0; i < 11; ++i)
         assert(r0[i] == expected_r0[i]);
+
+    // `search_position` example
+    int a1[4] = { 1, 3, 5, 6 };
+    assert(search_position(a1, 4, 5) == 2);
+
+    int a2[4] = { 1, 3, 5, 6 };
+    assert(search_position(a2, 4, 7) == 4);
 
     return 0;
 }
