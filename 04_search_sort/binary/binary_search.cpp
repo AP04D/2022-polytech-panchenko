23b1bffdb31faf8f6d3c7edcaebb0f0910e3cd39
AP04D <mrandpan2004@mail.ru>
2022-11-28 20:42:21 +0300

    feat: binary search task edited and fixed

--- 04_search_sort/binary/binary_search.cpp
+++ 04_search_sort/binary/binary_search.cpp
@@ -1,43 +1,24 @@
 #include <iostream>
 
 using namespace std;
-
-int binary_search(int arr[], int l, int r, int x)
+int bsearch(int arr[], int s, int e, int x)
 {
-    while (l <= r)
-    {
-        int mid = l + (r - l) / 2;
-        if (arr[mid] == x)
-        {
-            return mid;
-        }
-        if (arr[mid] < x)
-        {
-            l = mid + 1;
-        }
-        else
-        {
-            r = mid - 1;
-        }
-    }
-    return -1;
-}
-
-
-int main()
-{
-    int arr[5] = { 2, 3, 4, 10, 40 };
-    int x = 40;
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    int result = binary_search(arr, 0, n - 1, x);
-    if (result == -1)
-    {
-        std::cout << "Element is not present in array" << std::endl;
-    }
-    else
-    {
-        std::cout << "Element is present at index " << result << std::endl;
-    }
-    return 0;
+	if (e >= 1)
+	{
+		while (s <= e)
+		{
+			int m = (s + e) / 2;
+			if (arr[m] == x)
+				return m;
+			if (arr[m] < x)
+			{
+				s = m + 1;
+			}
+			if (arr[m] > x)
+			{
+				e = m - 1;
+			}
+		}
+	}
+	return -1;
 }
