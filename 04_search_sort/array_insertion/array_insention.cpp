0a256df5c851dcf69b1b5d793e64d4f7f411996c
AP04D <113236705+AP04D@users.noreply.github.com>
2022-12-04 23:50:24 +0300

    feat: array_insertion updated

--- 04_search_sort/array_insertion/array_insention.cpp
+++ 04_search_sort/array_insertion/array_insention.cpp
@@ -1,62 +1,66 @@
 #include <iostream>
-using namespace std;
-void insertion(int arr[], int n_arr[], int size, int a, int b)
-{
+#include <cstring>
+#include <cassert>
 
-    for (int i = 0; i <= size; i++)
-    {
+int A[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-        if (i == b)
-        {
+int *insert(int x, int p, int arr[], int n) {
+    for (int i = n; i > p; i--) arr[i] = arr[i - 1];
 
-            n_arr[i] = a;
-
-        }
-
-        if (i < b)
-        {
-
-            n_arr[i] = arr[i];
-
-        }
-
-        if (i > b)
-        {
-
-            n_arr[i] = arr[i - 1];
-
-        }
-
-    }
-
+    arr[p] = x;
+    return arr;
 }
-int main()
-{
 
-    int arr[] = { 1,43,12,2,4,12,4414141,111,31,22, };
-
-    int const size1 = 10;
-
-    int const size2 = 11;
-
-    int b, a;
-
-    int n_arr[size2];
-
-    cout << "Enter the number you wanna insert ";
-    cin >> a;
-    cout << "Enter the index for this element ";
-    cin >> b;
-    insertion(arr, n_arr, size1, a, b);
-    for (int i = 0; i < size2; i++)
-    {
-        cout << n_arr[i] << " ";
-
+int search_position(int x, int arr[], int n) {
+    for (int i = 0; i < n - 1; ++i) {
+        if (arr[i] >= x)
+            return i;
     }
+    return n;
+}
 
-    cout << endl << endl;
+int* insert_sorted(int arr[], int n, int x) {
+    int p = search_position(x, arr, n);
+    return insert(x, p, arr, n);
+}
 
-    system("pause 0");
+void print(int* arr, int n) {
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
 
+int main() {
+    std::cout << "First array: " << std::endl;
+    int a0[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int expected_r0[11] = {0, 1, 2, 3, 100, 4, 5, 6, 7, 8, 9};
+    print(a0, 10);
+    int* r0 = insert(100, 4, a0, 10);
+    for (int i = 0; i < 11; ++i) assert(r0[i] == expected_r0[i]);
+    print(a0, 11);
+    std::cout << std::endl;
+
+    std::cout << "Second array: " << std::endl;
+    int a1[4] = {1, 3, 5, 6};
+    print(a1, 4);
+    assert(search_position(5, a1, 4) == 2);
+    std::cout << "Position of 5: " << search_position(5, a1, 4) << std::endl;
+    std::cout << std::endl;
+    
+    std::cout << "Third array: " << std::endl;
+    int a2[4] = {1, 3, 5, 6};
+    print(a2, 4);
+    assert(search_position(7, a2, 4) == 4);
+    std::cout << "Position of 7: " << search_position(7, a2, 4) << std::endl;
+    std::cout << std::endl;
+    
+    std::cout << "Fourth array: " << std::endl;
+    int a3[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int expected_r3[11] = {0, 1, 2, 3, 4, 5, 5, 6, 7, 8, 9};
+    print(a3, 10);
+    int* r3 = insert_sorted(a3, 10, 5);
+    for (int i = 0; i < 11; ++i) assert(r3[i] == expected_r3[i]);
+    print(a3, 11);
     return 0;
 }
